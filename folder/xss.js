e020f903c102bce29c817aa18d1556381ef74697
jvstmer <57935669+jvstmer@users.noreply.github.com>
2022-06-19 16:38:25 -0500

    Update xss.js

--- folder/xss.js
+++ folder/xss.js
@@ -11,8 +11,8 @@ function reqListener() {
 */
 
 function cors() {
-  document.getElementById("report").innerHTML = alert("Sad");
   var xhr = new XMLHttpRequest();
+  document.getElementById("report").innerHTML = alert("Sad");
   xhr.onreadystatechange = function() {
     if (this.status == 200) {
       document.getElementById("report").innerHTML = alert(this.responseText);
